99253bda986e7fd6e4eb7ff40ff46f285e69e2cf
matteo <matteo.barbera97@gmail.com>
2019-07-24 18:07:12 +0200

    Implemented WLS method

--- MyLSPE.py
+++ MyLSPE.py
@@ -4,26 +4,31 @@ import numpy as np
 from numpy.linalg import inv
 
 
+# TODO add documentation
 class MyLSPE:
     class _Decorators:
         @classmethod
         def update_a_mat(cls, func):
             @functools.wraps(func)
-            def wrapper_update_a_mat(self, order=None):
-                if order is None or order == self._order:
-                    return func(self)
-                else:
-                    self._order = order
-                    self._gen_a_poly()
-                    return func(self)
+            def wrapper_update_a_mat(self, *args, **kwargs):
+                try:
+                    if kwargs['order'] == self._order:
+                        return func(self, *args, **kwargs)
+                    else:
+                        self._order = kwargs['order']
+                        self._gen_a_poly()
+                        return func(self, *args, **kwargs)
+                except KeyError:
+                    return func(self, *args, **kwargs)
+
             return wrapper_update_a_mat
 
     def __init__(self, x, y, order=1):
         if len(x) != len(y):
             raise ValueError("State and measurement data arrays need to be of equal length")
         self._N = len(x)
-        self._x = x.reshape(self._N, 1)
-        self._y = y.reshape(self._N, 1)
+        self._x = np.asarray(x)
+        self._y = np.asarray(y).reshape(self._N, 1)
         self._order = order
         self._gen_a_poly()
 
@@ -36,9 +41,21 @@ class MyLSPE:
 
     @_Decorators.update_a_mat
     def ols(self, order=None):
-        return np.matmul(np.matmul(inv(np.matmul(self._A.T, self._A)), self._A.T), self._y)
+        return inv(self._A.T @ self._A) @ self._A.T @ self._y
+
+    @_Decorators.update_a_mat
+    def wls(self, w_diag, order=None):
+        w_mat = np.diag(w_diag)
+        return inv(self._A.T @ inv(w_mat) @ self._A) @ self._A.T @ inv(w_mat) @ self._y
 
 
 if __name__ == "__main__":
     # TODO add unit tests
-    pass
+    x = list(range(10))
+    y = x
+    PE = MyLSPE(x, y)
+    print(PE.ols())
+    print(PE.ols(order=3))
+    w_diag = np.ones(10)
+    print(PE.wls(w_diag))
+    print(PE.wls(w_diag, order=2))
