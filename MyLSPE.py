b92bcb7fb1828589a36b97c2db49db3130600254
matteo <matteo.barbera97@gmail.com>
2019-07-24 16:55:47 +0200

    A(x) matrix now updates the member variable directly

--- MyLSPE.py
+++ MyLSPE.py
@@ -14,23 +14,25 @@ class MyLSPE:
                     return func(self)
                 else:
                     self._order = order
-                    self._A = self._gen_a_poly()
+                    self._gen_a_poly()
                     return func(self)
             return wrapper_update_a_mat
 
     def __init__(self, x, y, order=1):
-        self._N = len(x.flatten())
-        self._x = x
+        if len(x) != len(y):
+            raise ValueError("State and measurement data arrays need to be of equal length")
+        self._N = len(x)
+        self._x = x.reshape(self._N, 1)
         self._y = y.reshape(self._N, 1)
         self._order = order
-        self._A = self._gen_a_poly()
+        self._gen_a_poly()
 
     def _gen_a_poly(self):
         a_mat = np.empty((self._N, self._order + 1))
         a_mat[:, 0] = np.ones(self._N)
         for j in range(1, 1 + self._order):
             a_mat[:, j] = self._x ** j
-        return a_mat
+        self._A = a_mat
 
     @_Decorators.update_a_mat
     def ols(self, order=None):
@@ -38,14 +40,5 @@ class MyLSPE:
 
 
 if __name__ == "__main__":
-    x = np.asarray(list(range(10)))
-    y = x
-
-    PE = MyLSPE(x, y)
-    PE.ols()
-    print(PE._A)
-    PE.ols()
-    print(PE._A)
-
-
-
+    # TODO add unit tests
+    pass
