9440817b96e0356b6b240ad43b07c80c6df3fde4
matteo <matteo.barbera97@gmail.com>
2019-08-01 19:22:13 +0200

    Added method to compute global B-form regression matrix

--- simplex_bsplines.py
+++ simplex_bsplines.py
@@ -1,22 +1,28 @@
+from collections import defaultdict
 from itertools import product
 
 import numpy as np
+from scipy.sparse import coo_matrix, block_diag
 from scipy.spatial import Delaunay
 from scipy.special import factorial
 
 
 class SimplexSplines:
-    def __init__(self, x):
+    def __init__(self, x, y):
         self._dim = min(x.shape)
-        self._x = x.reshape(tuple(sorted(x.shape)))
+        self._x = x.reshape(tuple(sorted(x.shape)[::-1]))  # column major order
+        self._y = y
         self.tri = None
+
+        self._simp_xb_dict = defaultdict(list)
+        self._simp_y_dict = defaultdict(list)
 
     def _n_cube_domain(self, res):
         if res < 2:
             raise ValueError("Resolution must be at least 2")
         edge_points = []
         for n in range(self._dim):
-            x_n = self._x[n, :]
+            x_n = self._x[:, n]
             min_max = np.linspace(min(x_n), max(x_n), res)
             edge_points.append(min_max)
         mesh = np.meshgrid(*edge_points)
@@ -30,18 +36,24 @@ class SimplexSplines:
             points = custom_points
         self.tri = Delaunay(points)
 
-    def cart2bary(self, targets):
+    def _cart2bary(self, targets):
         # find index of simplices target points are in
         idx_simplices = self.tri.find_simplex(targets)
-        idx_simplices_valid = idx_simplices[idx_simplices != -1]  # remove points not in any simplex
+
+        mask = idx_simplices != -1
+        idx_simplices_valid = idx_simplices[mask]  # remove points not in any simplex
 
         # Affine transform defined by A b = x - v_0
         X = (self.tri.transform[idx_simplices_valid, :self._dim])  # A^-1
-        Y = targets - self.tri.transform[idx_simplices_valid, self._dim]  # x - v_0
+        Y = targets[mask] - self.tri.transform[idx_simplices_valid, self._dim]  # x - v_0
         b = np.einsum('ijk,ik->ij', X, Y)
         b_coords = np.c_[b, 1 - b.sum(axis=1)]
 
-        return b_coords
+        for s_idx, data_b, data_y in zip(idx_simplices_valid, b_coords, self._y[mask]):
+            self._simp_xb_dict[s_idx].append(data_b)
+            self._simp_y_dict[s_idx].append(data_y)
+            # TODO Store where each data point in cart coords is too?
+            # B-Spline coefficients should be already correctly sorted with B-mat, prob not needed
 
     @staticmethod
     def b_spline_func(d, b_coords):
@@ -57,10 +69,23 @@ class SimplexSplines:
         b_block *= b_coeffs  # B-form polynomial
         return b_block
 
+    def gen_b_rmat(self, d):
+        self._cart2bary(self._x)
+
+        b_blocks = []
+        sorted_y = []
+        for tj in range(self.tri.simplices.shape[0]):
+            sorted_b_coords = np.asarray(self._simp_xb_dict[tj])[:, np.argsort(self.tri.simplices[tj])]
+            b_blocks.append(coo_matrix(self.b_spline_func(d, sorted_b_coords)))
+            sorted_y += self._simp_y_dict[tj]
+
+        return block_diag(b_blocks), sorted_y
+
 
 if __name__ == "__main__":
     x = np.random.random(1000).reshape(2, 500)
-    ss = SimplexSplines(x)
+    y = np.random.random(500)
+    ss = SimplexSplines(x, y)
     res = 4
 
     # import matplotlib.pyplot as plt
@@ -68,18 +93,28 @@ if __name__ == "__main__":
     # points = ss._n_cube_domain(res)
     # plt.plot(points[:, 0], points[:, 1], ls="None", color='k', marker='x')
     # ss.triangulate(res=res)
-    # print(ss.tri.simplices)
     # plt.triplot(ss.tri.points[:, 0], ss.tri.points[:, 1], ss.tri.simplices)
     # plt.show()
 
     # Exercise 1 L06 Barycentric coordinate transformation
-    t_points = [[1, -1], [0, 0], [-1, -1]]
+    t_points = [[0, 0], [1, -1], [-1, -1]]
+    ss._y = np.asarray([1, 1])  # needed to make test work
     ss.triangulate(custom_points=t_points, res=res)
-    b_coords = ss.cart2bary([[0, -1], [0, -2 / 3]])
+    ss._cart2bary(np.asarray([[0, -1], [0, -2 / 3]]))
     answ = [[0, 0.5, 0.5], [1/3, 1/3, 1/3]]
-    assert((np.sort(b_coords) - answ).sum() < 1e-15)
+    assert((np.sort(ss._simp_xb_dict[0]) - answ).sum() < 1e-15)
 
     # Exercise 2 L06 Simplex polynomial evaluation
     b_coords = np.array([[0.5, 0.25, 0.25]])
     ex2 = SimplexSplines.b_spline_func(2, b_coords) * [2, -4, 4, -4, 3, 2]
     assert (ex2.sum() == 0.75)
+
+    # Example 6.8 L06 Estimating a linear simplex spline
+    x2 = np.asarray([[0.2, 0.8], [0.4, 0.2], [0.8, 0.2], [0.8, 0.6]])
+    y2 = np.asarray([-1, 2, 2, 1])
+    ss2 = SimplexSplines(x2, y2)
+    ss2.triangulate(custom_points=[[0, 1], [0, 0], [1, 0], [1, 1]])
+    b_regmat, sorted_y = ss2.gen_b_rmat(1)
+    answ = [-0.6, -0.2, -0.2, 1.4, 1.8, 1.8]
+    Bt_Y = b_regmat.T @ sorted_y
+    assert((np.sort(Bt_Y) - np.sort(answ)).sum() < 1e-15)
